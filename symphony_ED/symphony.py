4ca55ec4d1bcef833339f6e2dc4942c9092a465e
SuspensionRailway <timur@inf.u-szeged.hu>
2025-12-21 00:17:35 +0500

    Update symphony.py

--- symphony_ED/symphony.py
+++ symphony_ED/symphony.py
@@ -107,7 +107,7 @@ class SilentDropout(jit.ScriptModule):
 
 
 class Swaddling(jit.ScriptModule):
-    def __init__(self, beta=0.5):
+    def __init__(self):
         super(Swaddling, self).__init__()
 
     @jit.script_method
@@ -128,6 +128,7 @@ class Swaddling(jit.ScriptModule):
 class FeedForward(jit.ScriptModule):
     def __init__(self, f_in, h_dim, f_out):
         super(FeedForward, self).__init__()
+
 
         self.ffw = nn.Sequential(
             nn.Linear(f_in, h_dim),
@@ -165,6 +166,7 @@ class ActorCritic(jit.ScriptModule):
         self.qB = FeedForward(state_dim+action_dim, h_dim, q_nodes)
         self.qC = FeedForward(state_dim+action_dim, h_dim, q_nodes)
         self.qnets = nn.ModuleList([self.qA, self.qB, self.qC])
+
 
         self.q_dist = q_nodes*len(self.qnets)
         indexes = torch.arange(0, self.q_dist, 1)/self.q_dist
@@ -236,7 +238,7 @@ class Nets(jit.ScriptModule):
         nets_loss = -self.rehae((q_next_target - q_next_ema)/q_next_ema.abs()) + self.rehse(q_pred-q_target) + self.sw(next_scale, next_beta) 
         self.q_next_ema = q_next_ema.mean()
 
-        return nets_loss, next_scale.detach(), next_beta.detach()
+        return nets_loss, next_scale.detach()
 
 
 
@@ -273,14 +275,14 @@ class Symphony(object):
 
         state, action, reward, next_state, not_done_gamma = self.replay_buffer.sample(self.batch_size)
         self.nets_optimizer.zero_grad(set_to_none=True)
-        self.nets.tau_update()
-
-        nets_loss, scale, beta = self.nets(state, action, reward, next_state, not_done_gamma)
+        
+        nets_loss, scale = self.nets(state, action, reward, next_state, not_done_gamma)
 
         nets_loss.backward()
         self.nets_optimizer.step()
+        self.nets.tau_update()
 
-        return scale, beta
+        return scale
 
 
 
@@ -307,11 +309,11 @@ class ReplayBuffer:
 
         idx = self.length-1
 
-        self.states[idx,:] = torch.tensor(state, dtype=torch.float16, device=self.device)
-        self.actions[idx,:] = torch.tensor(action, dtype=torch.float16, device=self.device)
-        self.rewards[idx,:] = torch.tensor([reward/self.norm], dtype=torch.float16, device=self.device)
-        self.next_states[idx,:] = torch.tensor(next_state, dtype=torch.float16, device=self.device)
-        self.not_dones_gamma[idx,:] = torch.tensor([0.99 * (1.0 - float(done))], dtype=torch.float16, device=self.device)
+        self.states[idx,:] = torch.tensor(state, dtype=torch.float32, device=self.device)
+        self.actions[idx,:] = torch.tensor(action, dtype=torch.float32, device=self.device)
+        self.rewards[idx,:] = torch.tensor([reward/self.norm], dtype=torch.float32, device=self.device)
+        self.next_states[idx,:] = torch.tensor(next_state, dtype=torch.float32, device=self.device)
+        self.not_dones_gamma[idx,:] = torch.tensor([0.99 * (1.0 - float(done))], dtype=torch.float32, device=self.device)
 
         if self.length>=self.capacity:
             shift = 2 if self.not_dones_gamma[0,:].item() == 0.0 else 1
